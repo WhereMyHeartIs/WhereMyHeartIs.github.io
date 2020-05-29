f81e42d3bdc21eb001c9060f30ed51de6954cf2b
YouyouZzz <yzhao118@ucsc.edu>
2020-05-29 16:53:34 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './img/logo.png';
+import screen1 from './img/1.png';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+     return (
+        <div className="App">
+        <div  className="bg">
+        <h1>Where My Heart Is</h1>
+        <img src={logo} alt="Logo" />;
+        <h1>About Our Game</h1>
+        <p className="Intro">About the game place holder 1233333</p>
+        <img src={screen1} alt="screen1" />;
+        <h1>Game Play</h1>
+        <p className="Intro">More Game play gif</p>
+        <h1>Avaliable Now For PC</h1>
+        <p className="Intro">Windows and Mac Download link</p>
+        <h1>About the us</h1>
+        <p className="Intro">about us :D team members</p>
+        <h1>Random Title</h1>  
+        </div>    
+        </div>
+      );
+  }
+}
+function Header() {
+  // Import result is the URL of your image
+  return <img src={logo} alt="Logo" />;
 }
 
+
 export default App;
+
